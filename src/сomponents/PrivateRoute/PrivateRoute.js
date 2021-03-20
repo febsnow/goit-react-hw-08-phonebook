dc6b66486fc8cec71230596f78a021af777dbe11
Smekalin Sergei <crypto.mnr@gmail.com>
2021-03-21 01:10:09 +0200

    ничего существенного

--- src/сomponents/PrivateRoute/PrivateRoute.js
+++ src/сomponents/PrivateRoute/PrivateRoute.js
@@ -1,3 +1,4 @@
+import react from "react";
 import { connect } from "react-redux";
 import { Redirect, Route } from "react-router";
 import authSelectors from "../../redux/auth/auth-selectors";
--- src/сomponents/PublicRoute/PublicRoute.js
+++ src/сomponents/PublicRoute/PublicRoute.js
@@ -1,3 +1,4 @@
+import react from "react";
 import { connect } from "react-redux";
 import { Redirect, Route } from "react-router";
 import authSelectors from "../../redux/auth/auth-selectors";
@@ -5,13 +6,13 @@ import authSelectors from "../../redux/auth/auth-selectors";
 const PublicRoute = ({
   component: Component,
   redirectTo,
-  isAuthorized,
+  isAuthorised,
   ...routeProps
 }) => (
   <Route
     {...routeProps}
     render={(props) =>
-      isAuthorized && routeProps.restricted ? (
+      isAuthorised && routeProps.restricted ? (
         <Redirect to={redirectTo} />
       ) : (
         <Component {...props} />
@@ -21,7 +22,7 @@ const PublicRoute = ({
 );
 
 const mapStateToProps = (state) => ({
-  isAuthorized: authSelectors.getAuthorised(state),
+  isAuthorised: authSelectors.getAuthorised(state),
 });
 
 export default connect(mapStateToProps)(PublicRoute);
